38134620c9ba8695ce35a233b03e8442f3f084c7
andre-bandeli <andre.lbandeli@gmail.com>
2022-04-01 05:04:37 -0300

    feat: criacao forms + campo perguntas + select box

---
+++ src/Home.jsx
@@ -0,0 +1,319 @@
+import React from 'react'
+import './home.scss'
+
+
+export default function Home() {
+
+    return (
+        <div className='home'>
+            
+            <div className="box">
+                
+                
+                <div className="card">
+                    <div className="left">
+                        <div className="form-ask">
+                            <h2>Quão confortável você está com seus recursos financeiros?</h2>
+                        </div>
+                        
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                    <div className="right">
+                        <div className="form-ask">
+                            <h2>Você tem se dedicado a cuidar de suas finanças?</h2>
+                        </div>
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                   
+                </div>
+
+                <div className="card">
+                    <div className="left">
+                        <div className="form-ask">
+                            <h2>Quão confortável você está com seus recursos financeiros?</h2>
+                        </div>
+                        
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                    <div className="right">
+                        <div className="form-ask">
+                            <h2>Você tem se dedicado a cuidar de suas finanças?</h2>
+                        </div>
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="left">
+                        <div className="form-ask">
+                            <h2>Quão confortável você está com seus recursos financeiros?</h2>
+                        </div>
+                        
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                    <div className="right">
+                        <div className="form-ask">
+                            <h2>Você tem se dedicado a cuidar de suas finanças?</h2>
+                        </div>
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                </div>
+
+                <div className="card">
+                 <div className="left">
+                        <div className="form-ask">
+                            <h2>Quão confortável você está com seus recursos financeiros?</h2>
+                        </div>
+                        
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                    <div className="right">
+                        <div className="form-ask">
+                            <h2>Você tem se dedicado a cuidar de suas finanças?</h2>
+                        </div>
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="left">
+                        <div className="form-ask">
+                            <h2>Quão confortável você está com seus recursos financeiros?</h2>
+                        </div>
+                        
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                    <div className="right">
+                        <div className="form-ask">
+                            <h2>Você tem se dedicado a cuidar de suas finanças?</h2>
+                        </div>
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                </div>
+
+                <div className="card">
+                 <div className="left">
+                        <div className="form-ask">
+                            <h2>Quão confortável você está com seus recursos financeiros?</h2>
+                        </div>
+                        
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                    <div className="right">
+                        <div className="form-ask">
+                            <h2>Você tem se dedicado a cuidar de suas finanças?</h2>
+                        </div>
+                        <label htmlFor="" className='labelform'>
+                            <div className="container p-5">
+                                <select name="custom-select" id="cust">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>
+                            
+                        </label>
+                    </div>
+                </div>
+                <div className="result">
+                    <div className="divs">
+                            <button className='btn btn-danger'> Limpar respostas </button>
+                    </div>
+                    <div className="divs">
+                            <button className='btn btn-success'> Próxima página</button>
+                    </div>
+                    <div className="divs">
+                            <button className='btn btn-success'>Intruções de uso </button>
+                    </div>
+                </div>
+
+                
+
+                
+
+            </div>
+        </div>
+    )
+}
